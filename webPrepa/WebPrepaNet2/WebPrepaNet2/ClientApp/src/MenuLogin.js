69b334aa67a23117b4f49736b4419fa0585ca412
Alan de la Cruz <alan09delacruz@gmail.com>
2022-10-18 11:30:08 -0500

    proyecto web login

---
+++ webPrepa/WebPrepaNet2/WebPrepaNet2/ClientApp/src/MenuLogin.js
@@ -0,0 +1,24 @@
+﻿import React from "react";
+import "./styles/login.css";
+import { Button } from "reactstrap";
+
+function Login() {
+    return (
+        <div className="back" style={{ backgroundImage: "url(/imagenes/fondoPrepaNet.png)"  }}>
+            <div className="login-box">
+                <h1>Login PrepaNet</h1>
+                <div className="textbox">
+                    <i className="fa fa-user" aria-hidden="true" />
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div className="textbox">
+                    <i className="fa fa-lock" aria-hidden="true" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <Button className="button">Iniciar</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
