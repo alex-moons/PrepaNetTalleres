acbc136cec14fabbc5f6a68fe0c1b031113092a0
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2022-11-15 13:56:29 -0600

    Update notificacion.swift

--- AppMovilPrepanet/notificacion.swift
+++ AppMovilPrepanet/notificacion.swift
@@ -6,17 +6,24 @@
 //
 
 import UIKit
+import Firebase
 
 class notificacion: NSObject {
-    var title:String
-    var date:Date
-    var content:String
-    var coordi:String
+    var titulo:String
+    var fecha:Date
+    var contenido:String
+    var autor_id:DocumentReference
+    var taller_id:String
+    var campus_id:String
+    var grupo_id:String
     
-    init(title: String, date: Date, content: String, coordi:String) {
-        self.title = title
-        self.date = date
-        self.content = content
-        self.coordi = coordi
+    init(titulo: String, fecha: Date, contenido: String, autor_id:DocumentReference, taller_id:String, campus_id:String, grupo_id:String) {
+        self.titulo = titulo
+        self.fecha = fecha
+        self.contenido = contenido
+        self.autor_id = autor_id
+        self.taller_id = taller_id
+        self.campus_id = campus_id
+        self.grupo_id = grupo_id
     }
 }
